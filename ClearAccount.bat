6c68658d57f360652f44c05d776c661da6010720
gi0210 <gi0210@wins21.co.kr>
2019-01-11 13:30:59 +0900

    add modify Account for Windows because crash git account

---
+++ ClearAccount.bat
@@ -0,0 +1,49 @@
+
+@ECHO OFF
+setlocal
+
+:: init val
+rem desctination
+rem user
+rem pass
+rem flag
+
+set destination=git:https://github.com
+
+echo Clear Git Credentials
+echo.
+
+set /p user=input user id: 
+set /p pass=input passowrd: 
+echo The Info is User: %user%, Password: %pass%.
+echo.
+set /p flag=Continue? (Y/N)
+echo %flag%
+if /i %flag% == Y (
+  goto NEXT
+) else (
+  goto QUIT
+)
+
+:NEXT
+echo.
+echo ======= start =======
+echo.
+echo delete Credentials
+cmdkey /delete:%destination%
+echo.
+echo add Credentials
+cmdkey /generic:%destination% /user:%user% /pass:%pass%
+echo.
+echo read Credentials
+cmdkey /list: %destination%
+echo.
+echo ======= end =======
+GOTO:EOF
+
+:QUIT
+echo.
+echo ======= end =======
+GOTO:EOF
+
+
